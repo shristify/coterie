28f9062c46664929ce34bcf55f3232d2677fd488
Prajjwal Sharma <59313731+prajjwal-24@users.noreply.github.com>
2020-10-12 21:12:36 +0530

    added basic messaging functionality

---
+++ src/Messages.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Sidebar from "./Sidebar"
+import "./Messages.css"
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+import {Button, Input} from '@material-ui/core'
+import {auth} from "./firebase"
+import ChatRoom from "./ChatRoom"
+
+function Messages() {
+    const[user]=useAuthState(auth)
+
+    
+    return (
+        <div className="message">
+            <Sidebar/>
+            
+            <div className="messageBody">
+            <h1>i m a message</h1> 
+    { user?(<ChatRoom/>):(<Button 
+    
+    variant="contained" color="primary">Sign in</Button>)}</div>
+        </div>
+    )
+}
+
+export default Messages
