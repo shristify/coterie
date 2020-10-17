638fcdb1225677ad8b1b9d1e656288942421f75b
Shristi Singh <shristisingh812@gmail.com>
2020-10-17 19:28:28 +0530

    Update Trending.js

--- src/Trending.js
+++ src/Trending.js
@@ -1,19 +1,38 @@
-import React from 'react'
 import Sidebar from "./Sidebar"
 import "./Trending.css"
-
+import React,{Component} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Games from "./components/Games";
+import Stream from "./components/Streams";
+import GameStreams from "./components/GameStreams";
+import ReactDOM from "react-dom";
+import { withRouter } from 'react-router-dom'
+import Messages from "./Messages"
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 import Header from "./Header"
+import Header1 from "./components/Header1";
 
 function Trending() {
-    return (
-        <div className="trending">
-            <Sidebar/>
-            <div style={{width:"100%"}}>
-                <Header/>
-                <h1>HERE</h1>
-           {/*fetch twitch api stuff here*/}
-           </div>
-        </div>
+    return (       
+<div className="Trending container-fluid">
+
+<Sidebar/>
+<Header/>
+      <Router>
+        
+    <Header1 />
+    <Switch>
+        <Route exact path="/trending/top-streams" component={withRouter(Stream)} />
+      <Route path="/trending/game/:id" component={withRouter(GameStreams)} />
+      <Route exact path="/trending" component={withRouter(Games)} />
+      </Switch>    
+        </Router>
+     
+      </div>
+           
+        
     )
 }
 
