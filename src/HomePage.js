cbe9dcafe5f00c0c8f956dd54a83bff2ff56a474
Shristi Singh <shristisingh812@gmail.com>
2020-10-17 19:27:05 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,21 +2,15 @@ import React from 'react'
 import Sidebar from "./Sidebar"
 import "./HomePage.css"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import NavBar from "./NavBar"
 import Header from "./Header"
 function HomePage() {
     return (
         <div className="homePage">
-          <div class="columns">
-          <div class="column is-12">
+          
            <Header/>
-           </div>
-              <div class="column is-1"> 
+         
               <Sidebar/>
-              </div>
-            
-           </div>
-   
 {/*feed*/}
 {//{/*profile all the time, upload option here*/}
 //{/*top 3*/}
