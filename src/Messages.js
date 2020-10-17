451a4bc6df97db0afc072fe088c787ec3e3db91b
Shristi Singh <shristisingh812@gmail.com>
2020-10-17 11:48:21 +0530

    Update Messages.js

--- src/Messages.js
+++ src/Messages.js
@@ -6,18 +6,18 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
 import {Button, Input} from '@material-ui/core'
 import {auth} from "./firebase"
 import ChatRoom from "./ChatRoom"
-
+import Header from "./Header"
 function Messages() {
     const[user]=useAuthState(auth)
     return (
         <div className="message">
             <Sidebar/>
-            
+            <div style={{width:"100%"}}><Header/>
             <div className="messageBody">
             <h1>Send Messages</h1> 
-    { user?(<ChatRoom/>):(<Button 
-    
-    variant="contained" color="primary">Sign in</Button>)}</div>
+    { user?(<ChatRoom/>):
+    (<Button variant="contained" color="primary">Sign in</Button>)}</div>
+        </div>
         </div>
     )
 }
