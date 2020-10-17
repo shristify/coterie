45ddd1b312c5a500a0f5662044a5d0b943877107
Shristi Singh <shristisingh812@gmail.com>
2020-10-17 11:47:58 +0530

    Update Trending.js

--- src/Trending.js
+++ src/Trending.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Sidebar from "./Sidebar"
 import "./Trending.css"
+
+import Header from "./Header"
+
 function Trending() {
     return (
         <div className="trending">
             <Sidebar/>
+            <div style={{width:"100%"}}>
+                <Header/>
+                <h1>HERE</h1>
            {/*fetch twitch api stuff here*/}
+           </div>
         </div>
     )
 }
