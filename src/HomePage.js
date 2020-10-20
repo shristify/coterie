f6a7dc3a68ac7ac7213c2c697c79504833003c0e
Prajjwal Sharma <59313731+prajjwal-24@users.noreply.github.com>
2020-10-20 21:36:44 +0530

    added video player in homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,7 @@ import "./HomePage.css"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import NavBar from "./NavBar"
 import Header from "./Header"
+import VideoPlayer from "./VideoPlayer/VideoPlayer"
 function HomePage() {
     return (
         <div className="homePage">
@@ -15,7 +16,13 @@ function HomePage() {
 {//{/*profile all the time, upload option here*/}
 //{/*top 3*/}
     }
-        </div>
+    <div className="timePass">
+
+      <VideoPlayer url=
+      "https://firebasestorage.googleapis.com/v0/b/cotereie.appspot.com/o/images%2FVanilla%20Custard%20Cake%20_%20Eggless%20_%20Without%20oven.mp4?alt=media&token=2748214d-3117-424e-a8b4-2b577691d164"/>
+      
+    
+        </div></div>
      )
 }
 
